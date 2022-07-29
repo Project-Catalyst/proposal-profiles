2c0eb1dc4316084508f59e180389c0da5cc86fe0
JulianaBMattos <julianabarcellosmattos@gmail.com>
2022-07-29 13:14:42 +0100

    adjusted vue-config

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,12 @@
 const { defineConfig } = require('@vue/cli-service')
+let publicPath = ''
+if (process.env.APP_ENV === 'production') {
+  publicPath = '/proposal-profiles/'
+}
+if (process.env.APP_ENV === 'development') {
+  publicPath = '/'
+}
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  publicPath: publicPath
 })
